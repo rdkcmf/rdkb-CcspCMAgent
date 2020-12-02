2345249724c9da4aa82dd6f2f84c2507ff2de2fa
Arathy Vikram <ArathyVikram_Nair@comcast.com>
2020-12-02 16:43:42 +0530

    RDKB-32899 : Optimization of log enable and log level to improve boot time
    Reason for change: To read log enable/level from syscfg db during bootup
    Test Procedure: As mentioned in ticket
    Risks: None
    Signed-off-by: ArathyVikram_Nair@comcast.com
    Change-Id: I2bcfb740a2366ccdd8675a92cbff24cd2fe73a01

--- source/CMAgentSsp/ssp_main.c
+++ source/CMAgentSsp/ssp_main.c
@@ -50,6 +50,7 @@
 #endif
 //#include <docsis_ext_interface.h>
 #include "safec_lib_common.h"
+#include "syscfg/syscfg.h"
 
 #define DEBUG_INI_NAME "/etc/debug.ini"
 
@@ -529,12 +530,29 @@ int main(int argc, char* argv[])
 #endif
 
     cmd_dispatch('e');
-	#ifndef DISABLE_LOGAGENT
-	RDKLogEnable = GetLogInfo(bus_handle,"eRT.","Device.LogAgent.X_RDKCENTRAL-COM_LoggerEnable");
-	RDKLogLevel = (char)GetLogInfo(bus_handle,"eRT.","Device.LogAgent.X_RDKCENTRAL-COM_LogLevel");
-	CM_RDKLogLevel = GetLogInfo(bus_handle,"eRT.","Device.LogAgent.X_RDKCENTRAL-COM_CM_LogLevel");
-	CM_RDKLogEnable = (char)GetLogInfo(bus_handle,"eRT.","Device.LogAgent.X_RDKCENTRAL-COM_CM_LoggerEnable");
-	#endif
+    syscfg_init();
+    CcspTraceInfo(("CM_DBG:-------Read Log Info\n"));
+    char buffer[5] = {0};
+    if( 0 == syscfg_get( NULL, "X_RDKCENTRAL-COM_LoggerEnable" , buffer, sizeof( buffer ) ) &&  ( buffer[0] != '\0' ) )
+    {
+        RDKLogEnable = (BOOL)atoi(buffer);
+    }
+    memset(buffer, 0, sizeof(buffer));
+    if( 0 == syscfg_get( NULL, "X_RDKCENTRAL-COM_LogLevel" , buffer, sizeof( buffer ) ) &&  ( buffer[0] != '\0' ) )
+    {
+        RDKLogLevel = (ULONG )atoi(buffer);
+    }
+    memset(buffer, 0, sizeof(buffer));
+    if( 0 == syscfg_get( NULL, "X_RDKCENTRAL-COM_CM_LogLevel" , buffer, sizeof( buffer ) ) &&  ( buffer[0] != '\0' ) )
+    {
+        CM_RDKLogLevel = (ULONG)atoi(buffer);
+    }
+    memset(buffer, 0, sizeof(buffer));
+    if( 0 == syscfg_get( NULL, "X_RDKCENTRAL-COM_CM_LoggerEnable" , buffer, sizeof( buffer ) ) &&  ( buffer[0] != '\0' ) )
+    {
+        CM_RDKLogEnable = (BOOL)atoi(buffer);
+    }
+    CcspTraceInfo(("CM_DBG:-------Log Info values RDKLogEnable:%d,RDKLogLevel:%u,CM_RDKLogLevel:%u,CM_RDKLogEnable:%d\n",RDKLogEnable,RDKLogLevel,CM_RDKLogLevel, CM_RDKLogEnable ));
     // printf("Calling Docsis\n");
 
     // ICC_init();
